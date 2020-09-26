5801c22a613c04afe5feb763f7207bee11e06ba1
estaf <shubhamranadive@gmail.com>
2020-09-26 14:22:36 -0400

    Parsing

--- src/main/HttpLib.java
+++ src/main/HttpLib.java
@@ -1,4 +1,39 @@
-
+import java.io.IOException;
+import java.net.*;
 
 public class HttpLib {
+    public String host;
+    public String port;
+    public String path;
+
+    public void methodPassing1(String com, String url1) throws IOException {
+        URL url = new URL(url1);
+        switch(com) {
+            case "vg":
+                System.out.println("vg"+url);
+                break;
+            case "vp":
+                System.out.println("vp"+url);
+                break;
+            case "hg":
+                System.out.println("hg"+url);
+                break;
+            case "hp":
+                System.out.println("hp"+url);
+                break;
+            case "f":
+                System.out.println("f"+url);
+                break;
+            case "d":
+                System.out.println("d"+url);
+                break;
+            case "o":
+                System.out.println("o"+url);
+                break;
+            default:
+                System.out.println("Please Enter The Right Command !!!");
+                System.out.println("Use \"httpc help\" for more information about commands.");
+
+        }
+    }
 }
