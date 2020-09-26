c109f9e893e89599d7dead3d8fe83e46d76d5d47
Parth Patel <patelparth_28@yahoo.com>
2020-09-26 14:27:41 -0400

    HTTPC

--- src/main/Client.java
+++ src/main/Client.java
@@ -4,11 +4,63 @@ import java.io.*;
 
 public class Client {
 
-    public static void main(){
+    public static void main(String args[]) throws IOException {
+
+
+        System.out.println("Welcome to cURL command-Line");
+
+
+        testlib();
+
+
+
+    }
+
+    public static void testlib() throws IOException {
+
+        System.out.print(">");
+        Scanner sc = new Scanner(System.in);
+        String user_req = sc.nextLine();
+        String input[] = user_req.split(" ");
+        HttpLib lib = new HttpLib();
+
+
+        //if first word httpc
+        if(input[0].equals("httpc")){
+
+            //
+            if(input[1].equals("help")){
+
+                System.out.println("TO DO");
+                //Add the instructions
+                String input_url = "http://httpbin.org/get?course=networking&assignment=1";
+                lib.get(input_url);
+
+
+
+
+            }
+
+
+
+
+        }
+
+        //if first word not httpc
+        else{
+
+            System.out.println("httpc is the only recognized library");
+
+
+        }
+
+
 
 
 
 
 
     }
+
+
 }
--- src/main/HttpLib.java
+++ src/main/HttpLib.java
@@ -1,4 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.URL;
 
 
 public class HttpLib {
+
+    boolean verbose = false;
+    int port = 80;
+    PrintWriter pw;
+    BufferedReader br;
+    Socket s;
+    String host;
+
+
+
+    public HttpLib() throws IOException {
+    }
+
+    void makeConnection(String input_url) throws IOException {
+
+        URL url = new URL(input_url);
+        host = url.getHost();
+        System.out.println(host);
+        s = new Socket(host,port);
+        System.out.println("Connection made succesfully");
+    }
+
+    void get(String input_url) throws IOException {
+
+        makeConnection(input_url);
+
+        pw = new PrintWriter(s.getOutputStream());
+        String input_url1 = "GET /get?course=networking&assignment=1 HTTP/1.0\r\n" + "Host:httpbin.org\r\n" +"\r\n";
+        pw.write(input_url1);
+        pw.flush();
+        br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String output = br.readLine();
+        System.out.println("Output:"+output);
+        while(output !=null){
+            System.out.println(output);
+            output = br.readLine();
+
+        }
+
+    }
+
 }
