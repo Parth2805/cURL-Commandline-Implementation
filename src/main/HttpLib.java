a14b1d9a7ba82d4d3bda38bf7e625327a6d4510f
Parth Patel <patelparth_28@yahoo.com>
2020-09-27 13:40:04 -0400

    Merged

--- src/main/HttpLib.java
+++ src/main/HttpLib.java
@@ -15,27 +15,27 @@ public class HttpLib {
 
     public void methodPassing1(String com, String url1) throws IOException {
         URL url = new URL(url1);
-        switch(com) {
+        switch (com) {
             case "vg":
-                System.out.println("vg"+url);
+                System.out.println("vg" + url);
                 break;
             case "vp":
-                System.out.println("vp"+url);
+                System.out.println("vp" + url);
                 break;
             case "hg":
-                System.out.println("hg"+url);
+                System.out.println("hg" + url);
                 break;
             case "hp":
-                System.out.println("hp"+url);
+                System.out.println("hp" + url);
                 break;
             case "f":
-                System.out.println("f"+url);
+                System.out.println("f" + url);
                 break;
             case "d":
-                System.out.println("d"+url);
+                System.out.println("d" + url);
                 break;
             case "o":
-                System.out.println("o"+url);
+                System.out.println("o" + url);
                 break;
             default:
                 System.out.println("Please Enter The Right Command !!!");
@@ -43,45 +43,47 @@ public class HttpLib {
 
         }
     }
-
-    boolean verbose = false;
-    int port = 80;
-    PrintWriter pw;
-    BufferedReader br;
-    Socket s;
-    String host;
-
-
-
-    public HttpLib() throws IOException {
-    }
-
-    void makeConnection(String input_url) throws IOException {
-
-        URL url = new URL(input_url);
-        host = url.getHost();
-        System.out.println(host);
-        s = new Socket(host,port);
-        System.out.println("Connection made succesfully");
-    }
-
-    void get(String input_url) throws IOException {
-
-        makeConnection(input_url);
-
-        pw = new PrintWriter(s.getOutputStream());
-        String input_url1 = "GET /get?course=networking&assignment=1 HTTP/1.0\r\n" + "Host:httpbin.org\r\n" +"\r\n";
-        pw.write(input_url1);
-        pw.flush();
-        br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        String output = br.readLine();
-        System.out.println("Output:"+output);
-        while(output !=null){
-            System.out.println(output);
-            output = br.readLine();
-
-        }
-
-    }
-
 }
+
+    //Library other
+//    boolean verbose = false;
+//    int port = 80;
+//    PrintWriter pw;
+//    BufferedReader br;
+//    Socket s;
+//    String host;
+//
+//
+//
+//    public HttpLib() throws IOException {
+//    }
+//
+//    void makeConnection(String input_url) throws IOException {
+//
+//        URL url = new URL(input_url);
+//        host = url.getHost();
+//        System.out.println(host);
+//        s = new Socket(host,port);
+//        System.out.println("Connection made succesfully");
+//    }
+//
+//    void get(String input_url) throws IOException {
+//
+//        makeConnection(input_url);
+//
+//        pw = new PrintWriter(s.getOutputStream());
+//        String input_url1 = "GET /get?course=networking&assignment=1 HTTP/1.0\r\n" + "Host:httpbin.org\r\n" +"\r\n";
+//        pw.write(input_url1);
+//        pw.flush();
+//        br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+//        String output = br.readLine();
+//        System.out.println("Output:"+output);
+//        while(output !=null){
+//            System.out.println(output);
+//            output = br.readLine();
+//
+//        }
+//
+//    }
+//
+//}
